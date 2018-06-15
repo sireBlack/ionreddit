59a1333314fed401cd30a307d3462bc376b68444
sireBlack <dblacktin@gmail.com>
2018-06-15 23:18:52 +0000

    fixed angular bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+// import { AboutPage } from '../pages/about/about';
+// import { ContactPage } from '../pages/contact/contact';
+// import { HomePage } from '../pages/home/home';
+
+import { RedditPage } from '../pages/reddit/reddit';
+import { SettingsPage } from '../pages/settings/settings';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
+    RedditPage,
+    SettingsPage,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    RedditPage,
+    SettingsPage,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
--- src/pages/reddit/reddit.ts
+++ src/pages/reddit/reddit.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'about',
-  templateUrl: 'about.html'
+  selector: 'reddit',
+  templateUrl: 'reddit.html'
 })
-export class AboutPage {
+export class RedditPage {
 
   constructor(public navCtrl: NavController) {
 
